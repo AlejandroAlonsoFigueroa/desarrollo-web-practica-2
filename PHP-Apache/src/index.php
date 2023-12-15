ed902248d3d29e5010264bcf7f284f7d3397d26c
AlejandroAlonsoFigueroa <alejandro.figueroa.contractor@openpay.mx>
2023-12-14 22:05:58 -0600

    Agrega maqueta de formulario para agregar empleados

--- PHP-Apache/src/index.php
+++ PHP-Apache/src/index.php
@@ -8,6 +8,41 @@
         </style>
     </head>
     <body>
+
+    <div>
+        <form action = "guardarEmpleado.php" method = "POST">
+                <div>
+                    <div>
+                        <div><p>Clave</p></div>
+                        <div><input type = "number" name = "clave" required /></div>
+                    </div>
+                    <div>
+                    <div><p>Nombre</p></div>
+                        <div><input type = "text" name = "nombre"required/></div>
+                    </div>
+                    <div>
+                        <div><p>Dirección</p></div>
+                        <div><input type = "text" name = "direccion" required/></div>
+                    </div>
+                    <div>
+                        <div><p>Teléfono</p></div>
+                        <div><input type = "text" name = "telefono" required/></div>
+                    </div>
+                </div>
+                <div style = "margin-top: 10px;">
+                    <button type = "submit">Guardar empleado</button>
+                </div>
+        </form>
+    </div>
+        <?php 
+            function obtenerEmpleados(){
+                $sql = "SELECT clave, nombre, direccion, telefono FROM empleado";
+                $conexion =  obtenerConexionBBDD();
+                $stmt = $conexion->prepare($sql);
+                $stmt->execute();
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+        ?>
         <div>
             <h1>CRUD Empleados PHP</h1>
 
